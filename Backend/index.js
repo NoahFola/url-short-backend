2ff9041d55c2c424e5ce7f988b85283d425ae8fb
NoahFola <noahfola2005@gmail.com>
2025-02-24 07:46:33 +0100

    github actions

--- Backend/index.js
+++ Backend/index.js
@@ -8,11 +8,6 @@ const port = process.env.PORT
 
 const app = express()
 
-app.use((req, res, next) => {
-  req.url = `/url${req.url}`;
-  next();
- })
-
 
 app.use(express.json());
 app.use(cors());
