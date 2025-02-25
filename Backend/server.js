780dc3ff050871964c2a60212a64e9ea66de8b89
NoahFola <noahfola2005@gmail.com>
2025-02-25 05:22:04 +0100

    fixed jest issues 2

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const  express = require("express");
 const cors = require('cors');
 const url = require("./routes/url")
+const {dbSync} = require("./Backend/models/url")
 
 
 const app = express()
@@ -8,7 +9,7 @@ const app = express()
 app.use((req, res, next) => {
   req.url = `/url${req.url}`;
   next();
- })
+})
 
 
 app.use(express.json());
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 
 
 app.use("/url",  url)
+dbSync();
 
 module.exports = app;
 
